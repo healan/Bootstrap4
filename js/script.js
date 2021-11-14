881a1efd1597c353b7d9e51c0531a1f520d9915b
healan11 <healan11@nate.com>
2021-11-14 22:25:43 +0900

    grunt

---
+++ js/script.js
@@ -0,0 +1,25 @@
+<script>
+    $(document).ready(function(){
+        $("#mycarousel").carousel( { interval: 1000 } ); //time of change
+        $("#carouselButton").click(function(){
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                $("#mycarousel").carousel('pause');
+                $("#carouselButton").children("span").removeClass('fa-pause');
+                $("#carouselButton").children("span").addClass('fa-play');
+            }
+            else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');                    
+            }
+        });
+    });
+
+    $('#Login').on('click', function () {
+        $('#LoginModal').modal('show')
+    })
+
+    $('#Reserve').on('click', function () {
+        $('#ReserveModal').modal('show')
+    })
+</script>
